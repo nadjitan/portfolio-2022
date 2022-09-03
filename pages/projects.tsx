589982b591b02f74f5d7381d908e4e64efe00fcf
nadjitan <nadjitan@yahoo.com.ph>
2022-09-03 09:35:12 +0800

    change text

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,11 +9,7 @@ const Projects = () => {
       </Head>
 
       <div className="box-border flex h-full w-full flex-col overflow-y-auto overflow-x-hidden p-4">
-        <h1>projects</h1>{" "}
-        <p className="mt-2">
-          This site is made by me using NextJS and hosted in Vercel
-        </p>{" "}
-        <p>WIP</p>
+        <h1>projects</h1> <p className="mt-2">WIP</p>
       </div>
     </>
   )
--- pages/tech.tsx
+++ pages/tech.tsx
@@ -9,7 +9,11 @@ const Skills = () => {
       </Head>
 
       <div className="box-border h-full w-full p-4">
-        <h1>Tech</h1> <p>WIP</p>
+        <h1>Tech</h1>{" "}
+        <p className="mt-2">
+          This site is made by me using NextJS and hosted in Vercel
+        </p>{" "}
+        <p>WIP</p>
       </div>
     </>
   )
